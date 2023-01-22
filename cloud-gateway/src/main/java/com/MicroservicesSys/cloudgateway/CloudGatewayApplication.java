9726f6ac4c1d464f0df825ade0f96fe0bbd222dd
Abanoub-Asaad <abanoub.aeissa@orange.com>
2023-01-22 15:07:29 +0200

    add api gateway

---
+++ cloud-gateway/src/main/java/com/MicroservicesSys/cloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.MicroservicesSys.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
